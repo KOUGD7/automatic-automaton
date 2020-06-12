5060c6c7d086b6d9cce1799817e25d0291244cec
Zidane <zidane.wright@speurgroup.com>
2020-06-12 15:45:15 -0500

    add documentation, update method parameters and handle errors

--- server/plugins/preprocessor.py
+++ server/plugins/preprocessor.py
@@ -8,29 +8,41 @@ from core.model.preprocessor import Preprocessor
 class BasePreprocessor(Preprocessor):
 
     @staticmethod
-    def smooth(img):
+    def smooth(img, d: int = 7, sigma_color: int = 100, sigma_space: int = 100):
         """Use a bilateral filter to smooth the image while keeping edges sharp"""
 
-        d, sigma_color, sigma_space = 7, 100, 100
-        smooth = cv.bilateralFilter(img, d, sigma_color, sigma_space)
-        return smooth
+        try:
+            smooth_img = cv.bilateralFilter(img, d, sigma_color, sigma_space)
+            return smooth_img
+        except Exception as e:
+            raise(e)
 
     @staticmethod
     def convert_to_grayscale(img):
-        return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        try:
+            grayscale_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            return grayscale_image
+        except Exception as e:
+            raise(e)
 
     @staticmethod
-    def binarize(img):
+    def binarize(img, max_value: int = 255, block_size: int = 11, C: int = 3):
         """Convert a grayscale image to black and white using adaptive thresholding"""
 
-        max_value, block_size, C = 255, 11, 3
-        return cv.adaptiveThreshold(
-            img,
-            max_value,
-            cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-            cv.THRESH_BINARY_INV,
-            block_size,
-            C)
+        try:
+            binary_image = cv.adaptiveThreshold(
+                img,
+                max_value,
+                cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+                # Use THRESH_BINARY_INV so the foreground is white and the
+                # background is black. Necessary for applying morphological
+                # transformations like erosion and dilating
+                cv.THRESH_BINARY_INV,
+                block_size,
+                C)
+            return binary_image
+        except Exception as e:
+            raise(e)
 
     @staticmethod
     def close(img,
@@ -38,11 +50,17 @@ class BasePreprocessor(Preprocessor):
               iterations: int = 1):
         """Perform a closing morphological transformation on the input image"""
 
-        tmp = cv.dilate(img, kernel, iterations)
-        return cv.erode(tmp, kernel, iterations)
+        try:
+            dilated_img = cv.dilate(img, kernel, iterations)
+            eroded_img = cv.erode(dilated_img, kernel, iterations)
+            return eroded_img
+        except Exception as e:
+            raise(e)
 
     @staticmethod
     def preprocess(img):
-        tmp = BasePreprocessor.smooth(img)
-        tmp2 = BasePreprocessor.convert_to_grayscale(tmp)
-        return BasePreprocessor.binarize(tmp2)
+        """Run the preprocessing pipeline by smoothing, grayscaling then binarizing the input image in that order"""
+        smoothed_img = BasePreprocessor.smooth(img)
+        grayscale_img = BasePreprocessor.convert_to_grayscale(smoothed_img)
+        binary_img = BasePreprocessor.binarize(grayscale_img)
+        return binary_img
