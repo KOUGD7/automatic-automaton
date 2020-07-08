8f25dd038d7e10a7282826ec7c6cfc75b33a8fc1
Nathan Downer <nathandowner123@gmail.com>
2020-07-08 00:02:32 -0500

    run simulation on test data

--- client/src/pages/Simulator.tsx
+++ client/src/pages/Simulator.tsx
@@ -5,33 +5,39 @@ import {
 	IonToolbar,
 	IonTitle,
 	IonContent,
+	IonButton,
 } from '@ionic/react';
 import { GraphResponse, State, SimulationResponse } from '../models';
 import API from '../api';
+import data from '../assets/response.json';
 
 interface SimulatorPage {
 	photoName: string;
 }
 
+const stringsToTest = ['000000', '111111', '111101', '1111010', '010100'];
+
 const Simulator: React.FC<SimulatorPage> = ({ photoName }) => {
 	const [root, setRoot] = useState<State>();
 	const [stateList, setStateList] = useState<State[]>([]);
-	const [testStrings, setTestStrings] = useState<string[]>([]);
+	const [testStrings, setTestStrings] = useState<string[]>(stringsToTest);
 	const [simResponses, setSimResponses] = useState<SimulationResponse[]>([]);
 
 	useEffect(() => {
 		// send api call
 		(async () => {
-			const resp = await API.get('/associate-features/' + photoName);
-			const graph: GraphResponse = resp.data;
+			// const resp = await API.get('/associate-features/' + photoName);
+			// const graph: GraphResponse = resp.data;
+
+			const graph: GraphResponse = data;
 
 			// convert resp into stateList
 			let stateArr: State[] = graph.graph.map(g => {
 				const { label, centre, radius, is_accepting, transitions } = g;
 				return new State(label, centre, radius, is_accepting, transitions);
 			});
-			const rt = stateArr.find(st => st.is_root);
-			// setRoot(rt);
+			const rt = stateArr.find(st => st.label === graph.root);
+			setRoot(rt);
 			setStateList(stateArr);
 
 			// draw states and transitions
@@ -39,10 +45,13 @@ const Simulator: React.FC<SimulatorPage> = ({ photoName }) => {
 	}, []);
 
 	const runSimulation = () => {
+		setSimResponses([]);
+		let responses: SimulationResponse[] = [];
 		testStrings.forEach(ts => {
 			let simResp: SimulationResponse = simulateString(ts);
-			setSimResponses([...simResponses, simResp]);
+			responses.push(simResp);
 		});
+		setSimResponses(responses);
 	};
 
 	const simulateString = (testString: string): SimulationResponse => {
@@ -50,15 +59,20 @@ const Simulator: React.FC<SimulatorPage> = ({ photoName }) => {
 		/* possible outcomes
     string ends in an accept state -
     string ends in a non-accept state
-    string doesn't end but no transitions to make */
+    no more transitions and string didn't finish
+    no more transitions and string finished in accept
+    no more transitions and string didn't finish in accept state*/
 
 		let currentState: any = root; // used any instead of state to silence typescript
-		let nextStateLabel: number;
+		console.log('We start at: ', currentState.label);
 		for (let index = 0; index < testString.length; index++) {
 			let transitionBit = testString[index];
+			console.log('Next bit: ', transitionBit);
 			let next = currentState.next(transitionBit, stateList);
+			console.log('New state: ', next);
+
 			if (next === -1) {
-				// end in state
+				// no transitions
 				if (length - (index + 1) > 0) {
 					// check if there are more bits to process, therefore fail
 					return { was_accepted: false, stop_state: currentState.label };
@@ -69,11 +83,21 @@ const Simulator: React.FC<SimulatorPage> = ({ photoName }) => {
 					return { was_accepted: true };
 				}
 			} else {
-				// have more states to traverse
 				currentState = stateList.find(s => s.label === next);
+				// have more states to traverse
+				// check if string is finished
+				if (length - (index + 1) > 0) {
+					console.log('more bits to go!');
+					//have string left
+				} else if (currentState.is_accepting) {
+					// no string left
+					return { was_accepted: true };
+				} else {
+					return { was_accepted: false, stop_state: currentState.label };
+				}
 			}
 		}
-		return { was_accepted: false, stop_state: currentState };
+		return { was_accepted: false, stop_state: currentState.label };
 	};
 
 	return (
@@ -84,7 +108,18 @@ const Simulator: React.FC<SimulatorPage> = ({ photoName }) => {
 				</IonToolbar>
 			</IonHeader>
 			<IonContent>
-				<h1>simulator</h1>
+				<div>
+					<h2>Results</h2>
+					<ul>
+						{simResponses.map((sr, index) => (
+							<li key={index}>
+								{index}: {sr.was_accepted ? 'Passed!' : 'Failed'}{' '}
+								{!sr.was_accepted ? `at state ${sr.stop_state}` : ''}
+							</li>
+						))}
+					</ul>
+				</div>
+				<IonButton onClick={runSimulation}>Run SImulation</IonButton>
 			</IonContent>
 		</IonPage>
 	);
